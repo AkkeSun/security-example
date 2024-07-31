96ad181de72bcf2693f9686c0343099687b6d899
od <od@sweettracker.co.kr>
2024-07-31 17:35:59 +0900

    feat: springboot 3.0 적용 (SecurityConfig 수정)

--- src/main/java/com/sweettracker/securityexample/config/SecurityConfig.java
+++ src/main/java/com/sweettracker/securityexample/config/SecurityConfig.java
@@ -1,96 +1,84 @@
 package com.sweettracker.securityexample.config;
 
-import com.sweettracker.securityexample.service.MemberService;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+import java.util.Collections;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+/*
+    인증을 위한 DB 설정을 따로 하지 않아도 됩니다.
+ */
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    private final MemberService userDetailsService;
-    private final PasswordEncoder passwordEncoder;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        // --------------- 인증 정책 ---------------
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .formLogin() // form 로그인 사용
-            .loginPage("/login-page")              // 커스텀 로그인 페이지 url
-            .loginProcessingUrl("/login-process")  // 로그인 프로세싱 url (default = /login)
-            .usernameParameter("username")         // 로그인 아이디 파라미터명 (default = username)
-            .passwordParameter("password")         // 로그인 패스워드 파라미터명 (default = password)
-            .defaultSuccessUrl("/")
-            .failureUrl("/login-page?loginFail=true")
-        ;
-
-        // --------------- 인가 정책 ---------------
-        // 순서 : 1. 전체허용, 2. 아래로 갈수록 권한이 넓어지도록 설정
-        http
-            .authorizeRequests()
-            .antMatchers("/login-page").permitAll()
-            .antMatchers("/error/access-denied").permitAll()
-            .antMatchers("/user").hasRole("USER")
-            .antMatchers("/admin").hasRole("ADMIN")
-            .antMatchers("/user-and-admin").access("hasRole('USER') or hasRole('ADMIN')")
-            .anyRequest().authenticated()
-        //  .antMatchers("/test/ip").hasIpAddress("127.0.0.1")
-        //  .mvcMatchers(HttpMethod.GET, "shop/mvc").permitAll();
-        ;
-
-        //--------------- 인가 예외 처리 ---------------
-        http
-            .exceptionHandling()
-            .accessDeniedHandler(new AccessDeniedHandler() {
-                @Override
-                public void handle(HttpServletRequest request, HttpServletResponse response,
-                    org.springframework.security.access.AccessDeniedException accessDeniedException)
-                    throws IOException, ServletException {
-                    response.sendRedirect("/error/access-denied");
-
-                }
+            // --------------- 인증 정책 ---------------
+            .formLogin(login -> {
+                login.loginPage("/login-page")             // 커스텀 로그인 페이지 url
+                    .loginProcessingUrl("/login-process")  // 로그인 프로세싱 url (default = /login)
+                    .usernameParameter("username")         // 로그인 아이디 파라미터명 (default = username)
+                    .passwordParameter("password")         // 로그인 패스워드 파라미터명 (default = password)
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/login-page?loginFail=true");
             })
-        ;
 
-        //--------------- 로그아웃 설정 ---------------
-        http
-            .logout()
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login-page")
-            .deleteCookies("remember-me")
-        ;
+            // --------------- 인가 정책 ---------------
+            // 순서 : 1. 전체허용, 2. 아래로 갈수록 권한이 넓어지도록 설정
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/login-page").permitAll()
+                    .requestMatchers("/error/access-denied").permitAll()
+                    .requestMatchers("/user").hasRole("USER")
+                    .requestMatchers("/admin").hasRole("ADMIN")
+                    .anyRequest().authenticated();
+            })
 
-        //--------------- csrf, cors 설정 ---------------
-        http
-            .csrf().disable()
-            .cors();
+            //--------------- 인가 예외 처리 ---------------
+            .exceptionHandling(e -> {
+                e.accessDeniedHandler((request, response, accessDeniedException) ->
+                    response.sendRedirect("/error/access-denied"));
+            })
+
+            //--------------- 로그아웃 설정 ---------------
+            .logout(logout -> {
+                logout.logoutUrl("/logout")
+                    .logoutSuccessUrl("/login-page")
+                    .deleteCookies("remember-me");
+            })
+
+            //--------------- csrf, cors 설정 ---------------
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
+
+        ;
+        return http.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(Collections.singletonList("*"));
+            config.setAllowCredentials(true);
+            return config;
+        };
     }
 
 
-    @Override
-    // ============= DB로 사용자 정보 관리하기 위한 설정 =============
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    // ============= 정적파일 허용 =============
-    public void configure(WebSecurity web) {
-        web.ignoring()
+    // ------------ 정적파일 허용 ------------
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
