3483d6f6a09b7c81d974193ffafbfaa8b091ec9e
od <od@sweettracker.co.kr>
2024-07-31 17:36:14 +0900

    test: springboot 3.0 적용 (불필요한 의존성 제거)

--- src/test/java/com/sweettracker/securityexample/ControllerTestSupport.java
+++ src/test/java/com/sweettracker/securityexample/ControllerTestSupport.java
@@ -1,12 +1,9 @@
 package com.sweettracker.securityexample;
 
 import com.sweettracker.securityexample.config.SecurityConfig;
-import com.sweettracker.securityexample.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
@@ -15,10 +12,4 @@ public class ControllerTestSupport {
 
     @Autowired
     protected MockMvc mockMvc;
-
-    @MockBean
-    private MemberService userDetailsService;
-
-    @MockBean
-    private PasswordEncoder passwordEncoder;
 }
